d35a932c88a22d1a5408007e69b3042e1d4442ce
Eric Rogers <rogersgeric@gmail.com>
2020-02-28 22:15:36 -0500

    added parent nodes for iterative delete to work

--- Node.java
+++ Node.java
@@ -1,12 +1,14 @@
 public class Node{
     int data;
+    Node parent = null;
     Node right;
     Node left;
+	//public Object parent;
 
     public Node(int data){
         this.data = data;
         this.right = null;
         this.left = null;
     }
-    
+
 }
