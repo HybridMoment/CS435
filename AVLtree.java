23bef028a4cc99a316b0e08dba088fcfbcbaeb0d
Eric Rogers <rogersgeric@gmail.com>
2020-03-04 18:23:38 -0500

    insert rotates properly

--- AVLtree.java
+++ AVLtree.java
@@ -6,261 +6,8 @@ public class AVLtree{
     AVLtree(int data){
         this.root = new Node(data);
     }
-
-    //Recursive methods for AVL operations
-    public void insertRec(int data){
-        this.root = insertRecHelper(this.root, data);
-    }
-    private Node insertRecHelper(Node root, int data){
-        //base case
-        if(root == null){
-            root = new Node(data);
-            return root;
-        }
-        //case 1: recurse left sub tree
-        if(data < root.data){
-            //go left
-            root.left = insertRecHelper(root.left, data);
-            root.left.parent = root;
-            if(root.right == null){
-                //inserted into new level, update heights and balance factors
-                updateHeightAndBFInsertion(root);
-            }
-        }
-        //case 2: recurse right sub tree 
-        if(data > root.data){
-            //go right
-            root.right = insertRecHelper(root.right, data);
-            root.right.parent = root;
-            // just inserted, now we must correct heigh and balance factor, and perform rotations if needed
-            if(root.left == null){
-                //node inserted to new level, update height and balance factor of all parents
-                updateHeightAndBFInsertion(root);
-            }
-
-        }
-    
-        return root;
-    }
-    
-    private void updateHeightAndBFInsertion(Node root){
-
-        while(root != null){
-            root.height = calculateHeight(root);
-            root.balanceFactor = calculateBF(root);
-            if(root.balanceFactor >= 2 || root.balanceFactor <= -2){
-                rotateNode(root);
-            }
-            root = root.parent;
-        }
-
-    }
-
-    private int calculateHeight(Node root){
-
-        if(root.left != null && root.right == null){
-            return ( 1 + root.left.height);
-        }else if(root.left == null && root.right != null){
-            return ( 1 + root.right.height);
-        }else{
-            return( 1 + Math.max(root.left.height, root.right.height));
-        }
-        
-    }
-
-    private int calculateBF(Node cur){
-
-        if(cur.left != null && cur.right == null){
-            return (cur.left.height - 0);
-        }else if(cur.right != null && cur.left == null){
-            return (0 - cur.right.height);
-        }else{
-            return (cur.left.height - cur.right.height);
-        }
-
-    }
-
-    private void rotateNode(Node cur){
-        // perform rotations based on BF value
-        if(cur.balanceFactor >= 2 && cur.left.balanceFactor == -1){
-            //needs Left-Right rotation
-            System.out.println("CALLING LEFT RIGHT ROTATION");
-            leftRightRotation(cur);
-        }else if( cur.balanceFactor >= 2 && cur.left.balanceFactor == 1){
-            //needs Right rotation
-            System.out.println("CALLING RIGHT ROTATION");
-            //rightRotation(cur);
-        }else if(cur.balanceFactor <= -2 && cur.right.balanceFactor == 1){
-            //needs Right-Left rotation
-            System.out.println("CALLING RIGHT LEFT ROTAION");
-            //rightLeftRoatation(cur);
-        }else if(cur.balanceFactor <= -2 && cur.right.balanceFactor == -1){
-            // needs Left rotation
-            System.out.println("CALLING LEFT ROTATION");
-            //leftRotation(cur);
-        }
-       
-    }
-
-    private void rightRotation(Node cur){
-        Node next = cur.left;
-        Node a, b, c, treeZero, treeOne, treeTwo, treeThree;
-        c = cur;
-        b = next;
-        a = next.left;
-        treeZero = a.left;
-        treeOne = a.right;
-        treeTwo = b.right;
-        treeThree = c.right;
-        //Got nodes in right positions now rotate
-        if(cur.parent.left.data == cur.data){
-            cur.parent.left = b;   
-        }else{
-            cur.parent.right= b;
-        }
-        b.parent = cur.parent;
-        b.right = c;
-        b.left = a;
-        c.parent = b;
-        c.right = treeZero;
-        c.left = treeOne;
-        a.parent = b;
-        a.right = treeTwo;
-        a.left = treeThree;
-        //Fix height 
-    }
-
-    private void leftRotation(Node cur){
-        Node next = cur.right;
-        Node a, b, c, treeZero, treeOne, treeTwo, treeThree;
-        //Needs left rotation
-        a = cur;
-        b = next;
-        c = next.right;
-        treeZero = a.left;
-        treeOne = b.left;
-        treeTwo = c.left;
-        treeThree = c.right;
-        //got nodes in right positions now rotate 
-        if(cur.parent.right.data == cur.data){
-            cur.parent.right = b;   
-        }else{
-            cur.parent.left = b;
-        }
-        b.parent = cur.parent;
-        b.right = c;
-        b.left = a;
-        c.parent = b;
-        c.right = treeThree;
-        c.left = treeTwo;
-        a.parent = b;
-        a.left = treeZero;
-        a.right = treeOne;
-
-    }
-
-    private void rightLeftRoatation(Node cur){
-        Node next = cur.right;
-        Node a, b, c, treeZero, treeOne, treeTwo, treeThree;
-        //Needs right-left rotation
-        a = cur;
-        c = next;
-        b = next.left;
-        treeZero = a.left;
-        treeOne = b.left;
-        treeTwo = b.right;
-        treeThree = c.right;
-        //nodes in position now rotate
-        if(cur.parent.right.data == cur.data){
-            cur.parent.right = b;   
-        }else{
-            cur.parent.left = b;
-        }
-        b.parent = cur.parent;
-        b.right = c;
-        b.left = a;
-        c.parent = b;
-        c.right = treeThree;
-        c.left = treeTwo;
-        a.parent = b;
-        a.left = treeZero;
-        a.right = treeOne;
-
-    }
-
-    private void leftRightRotation(Node cur){
-        System.out.println("IN LEFT RIGHT ROTATION");
-        Node next = cur.left;
-        Node a, b, c, treeZero, treeOne, treeTwo, treeThree;
-        //Needs left-right rotation
-        c = cur;
-        a = next;
-        b = next.right;
-        /*
-        a.right = b.left;
-        c.left = b.right;
-        b.parent = c.parent;
-        b.left = a;
-        b.right = c;
-        a.parent = b;
-        c.parent = b;
-        */
-        System.out.println("c = "+b.data);
-    
-        if(cur.parent == null){
-            //rotation on root, update.
-            this.root = b;
-            System.out.println("updated root to : " + b.data);
-        }
-        
-        //System.out.println("Performing fake rotation");
-        //b.right = c;
-        //b.left = a;
-        //System.out.println("b = " + b.data);
-        //System.out.println("b.left = " + b.left.data);
-        //System.out.println("b.right = " + b.right.data);
-        
-        /*
-        treeZero = next.right;
-        treeOne = b.right;
-        treeTwo = b.left;
-        treeThree = a.left;
-        
-        //Got positions of all nodes and subtrees now rotate
-        b.parent = cur.parent;
-        b.right = c;
-        b.left = a;
-        a.parent = b;
-        a.left = treeThree;
-        a.right = treeTwo;
-        c.parent = b;
-        c.left = treeOne;
-        c.right = treeZero;
-        
-        //update heights and balance factors
-        c.height = calculateHeight(c);
-        c.balanceFactor = calculateBF(c);
-        a.height = calculateHeight(a);
-        a.balanceFactor = calculateBF(a);
-        b.height = calculateHeight(b);
-        b.balanceFactor = calculateBF(b);
-        if(parent != null){
-            //update parent node to point to new rotated sub-tree
-            if(parent.data < b.data){
-                parent.right = b;
-            }else{
-                parent.left = b;
-            }
-        }else{
-            //rotation happened on root, need to update
-            System.out.println("Updating root...");
-            this.root = b;
-        }
-        */
-    }
-
+     
     //Iterative methods for AVL operations
-
     public void insertIter(int data){
         Node cur = this.root;
         boolean notInserted = true;
@@ -283,7 +30,7 @@ public class AVLtree{
 
                     if(cur.left == null){
                         //inserted into a new level, need to update heights and balance factor of all parents
-                        updateHeightAndBFInsertion(cur.parent);
+                        updateHeightAndBFInsertion(cur);
                         break;
                     }
 
@@ -303,7 +50,7 @@ public class AVLtree{
 
                     if(cur.right == null){
                         //inserted into a new level, need to update heights and balance factor of all parents
-                        updateHeightAndBFInsertion(cur.parent);
+                        updateHeightAndBFInsertion(cur);
                         break;
                     }
 
@@ -313,7 +60,300 @@ public class AVLtree{
                 }
             }
         }
-        System.out.println("INSERT ITER : " + data);
+    }
+    
+    private void updateHeightAndBFInsertion(Node root){
+
+        while(root != null){
+            root.height = calculateHeight(root);
+            root.balanceFactor = calculateBF(root);
+            if(root.balanceFactor >= 2 || root.balanceFactor <= -2){
+                rotateNode(root);
+            }
+            root = root.parent;
+        }
+
+    }
+
+    private int calculateHeight(Node root){
+
+        if(root.left != null && root.right == null){
+            return ( 1 + root.left.height);
+        }else if(root.left == null && root.right != null){
+            return ( 1 + root.right.height);
+        }else if(root.left == null && root.right == null){
+            return 1;
+        }else{
+            return( 1 + Math.max(root.left.height, root.right.height));
+        }
+        
+    }
+
+    private int calculateBF(Node cur){
+
+        if(cur.left != null && cur.right == null){
+            return (cur.left.height - 0);
+        }else if(cur.right != null && cur.left == null){
+            return (0 - cur.right.height);
+        }else if(cur.right == null && cur.left == null){
+            return 0;
+        }else{
+            return (cur.left.height - cur.right.height);
+        }
+
+    }
+
+    private void rotateNode(Node cur){
+        // perform rotations based on BF value
+        if(cur.balanceFactor >= 2 && cur.left.balanceFactor == -1){
+            //needs Left-Right rotation
+            leftRightRotation(cur);
+        }else if( cur.balanceFactor >= 2 && cur.left.balanceFactor == 1){
+            //needs Right rotation
+            System.out.println("CALLING RIGHT ROTATION");
+            rightRotation(cur);
+        }else if(cur.balanceFactor <= -2 && cur.right.balanceFactor == 1){
+            //needs Right-Left rotation
+            System.out.println("CALLING RIGHT LEFT ROTAION");
+            rightLeftRoatation(cur);
+        }else if(cur.balanceFactor <= -2 && cur.right.balanceFactor == -1){
+            // needs Left rotation
+            System.out.println("CALLING LEFT ROTATION");
+            leftRotation(cur);
+        }
+       
+    }
+
+    private void rightRotation(Node cur){
+        Node next = cur.left;
+        Node a, b, c, treeZero, treeOne, treeTwo, treeThree;
+        int curData = cur.data;
+
+        //Set up nodes for manipulation, store subtrees that need to be moved
+        c = cur;
+        b = next;
+        a = next.left;
+        treeZero = a.left;
+        treeOne = a.right;
+        treeTwo = b.right;
+        treeThree = c.right;
+
+        //moving nodes for rotation
+        b.parent = cur.parent;
+        b.right = c;
+        b.left = a;
+        a.left = treeThree;
+        a.right = treeTwo;
+        c.left = treeOne;
+        c.right = treeZero;
+        a.parent = b;
+        c.parent = b;
+
+        //Check if rotation happened on root
+        Node parent = b.parent;
+        if(parent == null){
+            //rotation on root, update.
+            this.root = b;
+
+        }else{
+            //rotation did NOT occur on the root, must find if cur is a left or right child
+            if(parent.left != null && parent.left.data == curData){
+                //cur was a left child, we need to update the parent to point at new rotated node
+                parent.left = b;
+            }else{
+                //b must be a right child 
+                parent.right = b;
+            }
+        }
+
+        //Need to update heights and balance factors after performing rotations
+        c.height = calculateHeight(c);
+        c.balanceFactor = calculateBF(c);
+        a.height = calculateHeight(a);
+        a.balanceFactor = calculateBF(a);
+        b.height = calculateHeight(b);
+        b.balanceFactor = calculateBF(b);
+
+        //Need to update all parents since we performed a rotation
+        while(parent != null){
+
+            //recurse up parents and update their height and balance factor
+            parent.height = calculateHeight(parent);
+            parent.balanceFactor = calculateBF(parent);
+            parent = parent.parent;
+        }   
+
+    }
+
+    private void leftRotation(Node cur){
+        Node next = cur.right;
+        int curData = cur.data;
+        Node a, b, c, treeZero, treeOne, treeTwo, treeThree;
+        //Needs left rotation
+        a = cur;
+        b = next;
+        c = next.right;
+        treeZero = a.left;
+        treeOne = b.left;
+        treeTwo = c.left;
+        treeThree = c.right;
+
+        //got nodes in right positions now rotate 
+        b.parent = cur.parent;
+        b.right = c;
+        b.left = a;
+        c.parent = b;
+        c.right = treeThree;
+        c.left = treeTwo;
+        a.parent = b;
+        a.left = treeZero;
+        a.right = treeOne;
+
+        //Check if rotation took place on the root
+        Node parent = b.parent;
+        if(parent == null){
+            //rotation on root, update.
+            this.root = b;
+
+        }else{
+            //rotation did NOT occur on the root, must find if cur is a left or right child
+            if(parent.left != null && parent.left.data == curData){
+                //cur was a left child, we need to update the parent to point at new rotated node
+                parent.left = b;
+            }else{
+                //b must be a right child 
+                parent.right = b;
+            }
+        }
+        //Need to update heights and balance factors after performing rotations
+        c.height = calculateHeight(c);
+        c.balanceFactor = calculateBF(c);
+        a.height = calculateHeight(a);
+        a.balanceFactor = calculateBF(a);
+        b.height = calculateHeight(b);
+        b.balanceFactor = calculateBF(b);
+
+        //Need to update all parents since we performed a rotation
+        while(parent != null){
+            //recurse up parents and update their height and balance factor
+            parent.height = calculateHeight(parent);
+            parent.balanceFactor = calculateBF(parent);
+            parent = parent.parent;
+        }        
+
+    }
+
+    private void rightLeftRoatation(Node cur){
+        Node next = cur.right;
+        Node a, b, c, subtreeOne, subtreeTwo;
+        int curData = cur.data;
+
+        //Set up nodes for manipulation, store the subtrees that need to be moved
+        a = cur;
+        c = next;
+        b = next.left;
+        subtreeOne = b.left;
+        subtreeTwo = b.right;
+        
+        //moving nodes around to rotate
+        b.parent = cur.parent;
+        b.left = a;
+        b.right = c;
+        a.right = subtreeOne;
+        c.left = subtreeTwo;
+        a.parent = b;
+        c.parent = b;
+
+        //checking if rotation happened on root 
+        Node parent = b.parent;
+        
+        if(parent == null){
+            //rotation on node need to update
+            this.root = b;
+        }else{
+            //rotation did not happen on the root node
+            if(parent.left != null && parent.left.data == curData){
+                //cur was a left child, we need to update the parent to point at new rotated node
+                parent.left = b;
+            }else{
+                //b must be a right child 
+                parent.right = b;
+            }
+        }
+
+        //need to update heights and balance factors 
+        c.height = calculateHeight(c);
+        c.balanceFactor = calculateBF(c);
+        a.height = calculateHeight(a);
+        a.balanceFactor = calculateBF(a);
+        b.height = calculateHeight(b);
+        b.balanceFactor = calculateBF(b);
+
+        //Need to update all parents since we performed a rotation
+        while(parent != null){
+            //recurse up parents and update their height and balance factor
+            parent.height = calculateHeight(parent);
+            parent.balanceFactor = calculateBF(parent);
+            parent = parent.parent;
+        }
+
+    }
+
+    private void leftRightRotation(Node cur){
+        System.out.println("IN LEFT RIGHT ROTATION");
+        Node next = cur.left;
+        Node a, b, c, subtreeTwo, subtreeOne;
+        int curData = cur.data;
+
+        //Set up nodes for manipulation, store subtrees that need to be moved
+        c = cur;
+        a = next;
+        b = next.right;
+        subtreeTwo = b.left;
+        subtreeOne = b.right;
+
+        //Moving nodes around to rotate
+        b.parent = cur.parent;
+        b.left = a;
+        b.right = c;
+        a.right = subtreeTwo;
+        c.left = subtreeOne;
+        c.parent = b;
+        a.parent = b;
+
+        //Check if rotation took place on the root
+        Node parent = b.parent;
+        if(parent == null){
+            //rotation on root, update.
+            this.root = b;
+
+        }else{
+            //rotation did NOT occur on the root, must find if cur is a left or right child
+            if(parent.left != null && parent.left.data == curData){
+                //cur was a left child, we need to update the parent to point at new rotated node
+                parent.left = b;
+            }else{
+                //b must be a right child 
+                parent.right = b;
+            }
+        }
+        
+        //Need to update heights and balance factors after performing rotations
+        c.height = calculateHeight(c);
+        c.balanceFactor = calculateBF(c);
+        a.height = calculateHeight(a);
+        a.balanceFactor = calculateBF(a);
+        b.height = calculateHeight(b);
+        b.balanceFactor = calculateBF(b);
+
+        //Need to update all parents since we performed a rotation
+        while(parent != null){
+            //recurse up parents and update their height and balance factor
+            parent.height = calculateHeight(parent);
+            parent.balanceFactor = calculateBF(parent);
+            parent = parent.parent;
+        }
+
     }
     
     public void deleteIter(int data){
@@ -448,6 +488,7 @@ public class AVLtree{
     }
 
     private void transplantNode(Node u, Node v){
+        
         if(u.parent == null){
             //replace root with v
             this.root = v;
@@ -462,13 +503,23 @@ public class AVLtree{
     }
 
     public void inorder(Node root){
+        
         if(root != null){
             inorder(root.left);
             System.out.print(root.data + " ");
             inorder(root.right);
         }
     }
-
+    
+    public void postOrder(Node root){
+        
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
     
     
 }
